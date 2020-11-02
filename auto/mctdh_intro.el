abf9badea625cc36de98545614c06143d9c9449a
Panadestein <rpana92@gmail.com>
2020-11-02 18:48:56 +0100

    First full version

---
+++ auto/mctdh_intro.el
@@ -0,0 +1,63 @@
+(TeX-add-style-hook
+ "mctdh_intro"
+ (lambda ()
+   (TeX-add-to-alist 'LaTeX-provided-package-options
+                     '(("inputenc" "utf8") ("adjustbox" "export") ("algorithm2e" "ruled" "vlined")))
+   (add-to-list 'LaTeX-verbatim-environments-local "lstlisting")
+   (add-to-list 'LaTeX-verbatim-environments-local "semiverbatim")
+   (add-to-list 'LaTeX-verbatim-macros-with-braces-local "lstinline")
+   (add-to-list 'LaTeX-verbatim-macros-with-braces-local "href")
+   (add-to-list 'LaTeX-verbatim-macros-with-braces-local "hyperref")
+   (add-to-list 'LaTeX-verbatim-macros-with-braces-local "hyperimage")
+   (add-to-list 'LaTeX-verbatim-macros-with-braces-local "hyperbaseurl")
+   (add-to-list 'LaTeX-verbatim-macros-with-braces-local "nolinkurl")
+   (add-to-list 'LaTeX-verbatim-macros-with-braces-local "url")
+   (add-to-list 'LaTeX-verbatim-macros-with-braces-local "path")
+   (add-to-list 'LaTeX-verbatim-macros-with-delims-local "lstinline")
+   (add-to-list 'LaTeX-verbatim-macros-with-delims-local "path")
+   (TeX-run-style-hooks
+    "latex2e"
+    "beamer"
+    "beamer10"
+    "inputenc"
+    "graphicx"
+    "ragged2e"
+    "braket"
+    "xcolor"
+    "bm"
+    "adjustbox"
+    "algorithm2e"
+    "listings"
+    "caption"
+    "subcaption")
+   (TeX-add-symbols
+    '("norm" 1)
+    '("img" 1)
+    "oldmacro")
+   (LaTeX-add-labels
+    "nqd"
+    "stdmeth"
+    "stme"
+    "tdh"
+    "mctdh"
+    "mctdh_antz"
+    "eom"
+    "spf"
+    "mctdh_eom"
+    "mctdh_eom_mod_dec"
+    "mctdh_antz_mc"
+    "relax"
+    "mctdh_eom_ti"
+    "probpes"
+    "tdec"
+    "potfit"
+    "core"
+    "alg"
+    "sopfbr"
+    "applic"
+    "figpyr"
+    "fighh"
+    "figapl"
+    "biblio"))
+ :latex)
+
